76968936e1ad19c4b50135f51ca97da0c9fddb5b
tarh <tarh.x2@gmail.com>
2015-02-11 21:37:09 +0100

    0.13.1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    var o = ['m', '.', ':', 't', '@', 'g', 'ail'];
+    var addr = o[0] + o[6] + o[3] + 'o' + o[2] + o[3] + 'arh' + o[1] + 'x2' + o[4] + o[5] + o[0] + o[6] + o[1] + 'com';
+    var s = '<a class="BlockLink" href="' + addr + '">E-' + o[0] + o[6] + '</a>';
+    var list = document.getElementsByClassName('ph-email');
+    console.log(list);
+    for (var i = 0, len = list.length; i < len; ++i)
+        list[i].innerHTML = s;
+};
+//# sourceMappingURL=main.js.map
